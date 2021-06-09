5cb5e706d9fe68be44166179fd598eb6d767442c
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-06-09 17:16:05 +0300

    Added todo add feature

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const todoRoutes = require('./routes/todo');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/todo', todoRoutes);
--- routes/todo.js
+++ routes/todo.js
@@ -3,15 +3,45 @@ const Todo = require('../models/todo');
 const router = Router();
 
 // Получение списка задач
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+	try {
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({ message: 'Server error' });
+	}
+});
 
 // Создание новой задачи
-router.post('/', (req, res) => {});
+router.post('/', async (req, res) => {
+	try {
+		const todo = await Todo.create({
+			title: req.body.title,
+			done: false,
+		});
+
+		res.status(201).json({ todo });
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({ message: 'Server error' });
+	}
+});
 
 // Изменение задачи
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+	try {
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({ message: 'Server error' });
+	}
+});
 
 // Удаление задачи
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+	try {
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({ message: 'Server error' });
+	}
+});
 
 module.exports = router;
